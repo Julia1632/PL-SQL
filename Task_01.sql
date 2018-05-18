a16cd0ec56d552163a3d280d9a90d2460be5ae3b
yzhurun <yuliya.zhurun@intexsoft.by>
2018-05-18 17:49:20 +0300

    fix test and trigger

--- Task_01.sql
+++ Task_01.sql
@@ -10,24 +10,31 @@ if :new.price is null or :new.qty is null
 then :new.sum:=0;
  :new.nds:=0;
 else :new.sum:=:new.qty*:new.price;
-:new.nds:=:new.sum-(:new.price*20/120);
+:new.nds:=:new.sum*20/100;
 end if;
 
 
 end sum_nds_for_supply_str;
 /
 
-create procedure test_trigger_sum_nds_for_supply_str
+create or replace procedure test_tr_sum_nds
 as
+answer_s t_supply_str.sum%type;
+answer_n t_supply_str.nds%type;
 begin
 commit;
-insert into t_supplier(id_supplier,moniker) values (1111,'1111');
-insert into t_supply(id_supply,code,dt,id_supplier,e_state) values
-(1111,'11',sysdate,1111,'new');
-insert into t_ware(id_ware,moniker,name) 
-values (1111,'Ware_1','Ware_1');
-insert into t_supply_str (id_supply_str,id_supply,id_ware)
-values (1111,1111,1111);
 
-end test_trigger_sum_nds_for_supply_str;
+insert into t_supply_str (id_supply_str,id_supply,id_ware,qty,price)
+values (1112,2001,5001,10,to_number(10.2));
+select sum into answer_s from t_supply_str where id_supply_str=1112;
+dbms_output.put_line('correct value=102 Get'||to_char(answer_s));
+select nds into answer_n from t_supply_str where id_supply_str=1112;
+dbms_output.put_line('correct value=20.4 Get'||to_char(answer_n));
+rollback;
+end test_tr_sum_nds;
+/
+
+begin
+test_tr_sum_nds();
+end;
 /

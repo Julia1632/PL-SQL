e8532d5fb7f97c79b70ee15f4dd411f10a53d7e9
yzhurun <yuliya.zhurun@intexsoft.by>
2018-05-21 10:43:26 +0300

    last change

--- trigger.sql
+++ trigger.sql
@@ -41,16 +41,3 @@ END lock_table_t_supply;
 /
 
 
-
-insert into t_supply_str (id_supply_str,id_supply ,id_ware) 
-select 6005,2001,5001 
-from dual
-union all
-select 6006,2001,5001 
-from dual
-union all
-select 6007,2002,5001 
-from dual
-union all
-select 6008,2002,5001
-from dual;

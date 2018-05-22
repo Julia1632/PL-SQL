c9b79e4e8ea7933391e17ceb1042c9a091fd3fef
yzhurun <yuliya.zhurun@intexsoft.by>
2018-05-22 10:34:32 +0300

    create query

--- QUERY_08.sql
+++ QUERY_08.sql
@@ -1,2 +1,18 @@
-query-08
-Вычислить число строк продаж, количество, сумму средневзвешенную, максимальную и минимальную скидку по торговой марке (t_model.label). 
+/*query-08
+ВЫЧИСЛИТЬ ЧИСЛО СТРОК ПРОДАЖ, КОЛИЧЕСТВО, СУММУ СРЕДНЕВЗВЕШЕННУЮ, 
+максимальную и минимальную скидку по торговой марке (t_model.label). */
+
+  
+select m.label,
+  COUNT(ID_SALE_str) COUNT_S,
+  SUM(SUM) SUM_S,
+  SUM(SUM)/COUNT(ID_SALE) v_sum_s,
+  MAX(DISCOUNT) max_d,
+  MIN(DISCOUNT) min_d
+from T_SALE_STR S
+left JOIN T_WARE W
+on S.ID_WARE=W.ID_WARE
+ left join T_MODEL M
+on W.ID_MODEL=M.ID_MODEL
+group by m.label
+;

73a1749c879dcee98060944af955cc2cdd7a68b9
yzhurun <yuliya.zhurun@intexsoft.by>
2018-05-23 15:06:45 +0300

    create query

--- QUERY_11.sql
+++ QUERY_11.sql
@@ -1,14 +1,36 @@
+/*НАЙТИ МОДЕЛИ, НА КОТОРЫЕ 
+предоставлены нехарактерно большие скидки,
+с учетом предыдущего анализа. Сделать предположение о с составе этих моделей
+*/
 
-SELECT DISCOUNT FROM
-(
-select max(C)  MAX
+SELECT id_model,
+  z2.discount
 FROM
-(SELECT  distinct discount,  COUNT(id_sale_str) over (partition BY discount) C
-FROM T_SALE_STR SS
-join T_WARE W
-on W.ID_WARE=SS.ID_WARE)),
-(SELECT  distinct discount,  COUNT(id_sale_str) over (partition BY discount) C
-FROM T_SALE_STR SS
-join T_WARE W
-on W.ID_WARE=SS.ID_WARE) 
-WHERE MAX=C;
+  (SELECT DISCOUNT
+  FROM
+    (SELECT MAX(C) MAX
+    FROM
+      (SELECT DISTINCT discount,
+        COUNT(id_sale_str) over (partition BY discount) C
+      FROM T_SALE_STR SS
+      JOIN T_WARE W
+      ON W.ID_WARE=SS.ID_WARE
+      )
+    ),
+    (SELECT DISTINCT discount,
+      COUNT(id_sale_str) over (partition BY discount) C
+    FROM T_SALE_STR SS
+    JOIN T_WARE W
+    ON W.ID_WARE=SS.ID_WARE
+    )
+  WHERE MAX=C
+  ) Z1,
+  (SELECT m.id_model,
+    discount
+  FROM T_WARE T
+  JOIN T_MODEL M
+  ON M.ID_MODEL=T.ID_MODEL
+  JOIN T_SALE_STR SS
+  ON SS.ID_WARE=T.ID_WARE
+  ) Z2
+WHERE z2.discount>z1.discount ;

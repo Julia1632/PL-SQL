b77a94cc39e3d35caa33421de49d02004a0847e2
yzhurun <yuliya.zhurun@intexsoft.by>
2018-05-24 11:31:06 +0300

    create trigger only for  table t_rest

--- Task_14_new_version.sql
+++ Task_14_new_version.sql
@@ -1,37 +1,38 @@
+-- правильно но толко для t_rest
+
 CREATE OR REPLACE TRIGGER T_N_ADD_REST AFTER
-  update of E_STATE on T_SUPPLY for each row begin 
-  
-  if :old.E_STATE='new' and :new.E_STATE='done'
-  then
-  merge
-  into T_REST P1 using
-    ( SELECT id_ware, qty FROM T_SUPPLY_STR  WHERE ID_SUPPLY=:new.id_supply
-    ) P2 on (P2.ID_WARE=P1.ID_WARE) when matched then
-  update set P1.QTY=P1.QTY+p2.QTY when not matched then
-  INSERT 
-    ( ID_WARE, QTY
-    )
-    values (p2.id_ware,p2.qty)
-  ;
-  
-  end if;
-  
-  
-  
-   if :new.E_STATE='new' and :old.E_STATE='done'
-  then
-  merge
-  into T_REST P1 using
-    ( SELECT id_ware, qty FROM T_SUPPLY_STR  WHERE ID_SUPPLY=:new.id_supply
-    ) P2 on (P2.ID_WARE=P1.ID_WARE) when matched then
-  
-  update set P1.QTY=P1.QTY-p2.qty;
-  
-  end if;
- END T_N_ADD_REST;
-/ 
+  UPDATE OF E_STATE ON T_SUPPLY FOR EACH row DECLARE flag NUMBER;
+  BEGIN
+    IF :old.E_STATE='new' AND :new.E_STATE='done' THEN
+      merge INTO T_REST P1 USING
+      ( SELECT id_ware, qty FROM T_SUPPLY_STR WHERE ID_SUPPLY=:new.id_supply
+      ) P2 ON (P2.ID_WARE=P1.ID_WARE)
+    WHEN matched THEN
+      UPDATE SET P1.QTY=P1.QTY+p2.QTY WHEN NOT matched THEN
+      INSERT
+        (
+          ID_WARE,
+          QTY
+        )
+        VALUES
+        (
+          P2.ID_WARE,
+          P2.QTY
+        ) ;
+    END IF;
+    IF :new.E_STATE='new' AND :old.E_STATE='done' THEN
+      merge INTO T_REST P1 USING
+      ( SELECT id_ware, qty FROM T_SUPPLY_STR WHERE ID_SUPPLY=:new.id_supply
+      )
+      P2 ON (P2.ID_WARE=P1.ID_WARE)
+    WHEN matched THEN
+      UPDATE SET P1.QTY=P1.QTY-p2.qty;
+    END IF;
+  END T_N_ADD_REST;
+  / 
 
 
 select * from t_rest;
 
 delete from t_rest;
+
